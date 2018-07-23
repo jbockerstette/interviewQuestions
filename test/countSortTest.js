462bd270a0d90b40555bf3303b091ee4d6948c75
jbockerstette <jim.bockerstette@gmail.com>
2018-07-23 08:50:34 -0700

    Count sort solved.

--- test/countSortTest.js
+++ test/countSortTest.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const { sortScores } = require("../src/countSort");
-describe.only("countSort", () => {
+describe("countSort", () => {
   const HIGHEST_POSSIBLE_SCORE = 100;
   it("should sort", () => {
     const unsortedScores = [37, 89, 41, 65, 91, 91, 41, 53];
